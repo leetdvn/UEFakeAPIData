2832cd08c76ac8e6b165b7ae731f66003938b782
leetdvn <111244237+leetdvn@users.noreply.github.com>
2023-10-30 12:04:18 +0700

    Create AvatarFake.js

---
+++ AvatarFake.js
@@ -0,0 +1,165 @@
+{
+	"code": 200,
+	"message": "Done",
+	"status": true,
+	"totalCount": 2,
+	"limit": 50,
+	"totalPage": 1,
+	"page": 1,
+	"data": [
+		{
+			"id": 467,
+			"user_id": 495,
+			"token_id": 1505,
+			"name": "Avatar 1443",
+			"staking_flg": false,
+			"lending_flg": false,
+			"chain_id": 11155111,
+			"image_url": "https://d3dax4s58tii0r.cloudfront.net/photo/avatar/FEMALE_23_29_37_65_105_132_16_84.png",
+			"attributes": [
+				{
+					"id": 314,
+					"created_at": "2023-10-22T15:34:02Z",
+					"updated_at": "2023-10-22T15:34:02Z",
+					"deleted_at": null,
+					"attribute": 5,
+					"value": "FEMALE"
+				},
+				{
+					"id": 422,
+					"created_at": "2023-10-22T15:34:02Z",
+					"updated_at": "2023-10-22T15:34:02Z",
+					"deleted_at": null,
+					"attribute": 5,
+					"value": "FEMALE"
+				}
+			],
+			"avatar_parts": [
+				{
+					"id": 16,
+					"name": "Female-Choker",
+					"pattern": "FEMALE",
+					"part_type": 1
+				},
+				{
+					"id": 23,
+					"name": "Female_Base-01",
+					"pattern": "FEMALE",
+					"part_type": 2
+				},
+				{
+					"id": 29,
+					"name": "Female_Base-01",
+					"pattern": "FEMALE",
+					"part_type": 3
+				},
+				{
+					"id": 37,
+					"name": "Set_Female_Fur-Coat_Body",
+					"pattern": "FEMALE",
+					"part_type": 4
+				},
+				{
+					"id": 65,
+					"name": "Female_Eyes-With-Neon-Eyeshadow",
+					"pattern": "FEMALE",
+					"part_type": 5
+				},
+				{
+					"id": 84,
+					"name": "Female_Quantum-Technology-Device",
+					"pattern": "FEMALE",
+					"part_type": 6
+				},
+				{
+					"id": 105,
+					"name": "Female_Hair-Stuck",
+					"pattern": "FEMALE",
+					"part_type": 8
+				},
+				{
+					"id": 132,
+					"name": "Female_Joint-Mouth",
+					"pattern": "FEMALE",
+					"part_type": 10
+				}
+			]
+		},
+		{
+			"id": 466,
+			"user_id": 495,
+			"token_id": 533,
+			"name": "MALE_25_31_43_76_119_146_18_91_96",
+			"staking_flg": false,
+			"lending_flg": false,
+			"chain_id": 80001,
+			"image_url": "https://d3dax4s58tii0r.cloudfront.net/photo/avatar/MALE_25_31_43_76_119_146_18_91_96.png",
+			"attributes": [
+				{
+					"id": 426,
+					"created_at": "2023-10-22T15:10:02Z",
+					"updated_at": "2023-10-22T15:10:02Z",
+					"deleted_at": null,
+					"attribute": 5,
+					"value": "MALE"
+				}
+			],
+			"avatar_parts": [
+				{
+					"id": 18,
+					"name": "Male_Choker-With-Pendant",
+					"pattern": "MALE",
+					"part_type": 1
+				},
+				{
+					"id": 25,
+					"name": "Male_Base-03",
+					"pattern": "MALE",
+					"part_type": 2
+				},
+				{
+					"id": 31,
+					"name": "Male_Base-03",
+					"pattern": "MALE",
+					"part_type": 3
+				},
+				{
+					"id": 43,
+					"name": "Set_Male_Black-Oversize-Sweatshirt_Body",
+					"pattern": "MALE",
+					"part_type": 4
+				},
+				{
+					"id": 76,
+					"name": "Male_Neon-Eyes",
+					"pattern": "MALE",
+					"part_type": 5
+				},
+				{
+					"id": 91,
+					"name": "Male_Quantum-Technology-Device",
+					"pattern": "MALE",
+					"part_type": 6
+				},
+				{
+					"id": 96,
+					"name": "Male_Augmented-Reality-Glasses",
+					"pattern": "MALE",
+					"part_type": 7
+				},
+				{
+					"id": 119,
+					"name": "Male_Silver-Hair",
+					"pattern": "MALE",
+					"part_type": 8
+				},
+				{
+					"id": 146,
+					"name": "Male_Viking-Beard",
+					"pattern": "MALE",
+					"part_type": 10
+				}
+			]
+		}
+	]
+}
