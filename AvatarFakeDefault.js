ed6c7d51fe843a9f75bc3ce5b9fa2a3c35c92553
leetdvn <111244237+leetdvn@users.noreply.github.com>
2023-10-30 12:23:04 +0700

    Create AvatarFakeDefault.js

---
+++ AvatarFakeDefault.js
@@ -0,0 +1,517 @@
+{
+	"code": 200,
+	"message": "Done",
+	"status": true,
+	"totalCount": 22,
+	"limit": 50,
+	"totalPage": 1,
+	"page": 1,
+	"data": [
+		{
+			"id": 123,
+			"name": "Set_Female_High-class-suit-02_Body",
+			"image_url": "https://d1zyql4aht6ss7.cloudfront.net/images/avatars/F_NPC_02_1_1698256644348.png",
+			"attributes": [
+				{
+					"id": 466,
+					"created_at": "2023-10-25T17:57:34Z",
+					"updated_at": "2023-10-25T17:57:34Z",
+					"deleted_at": null,
+					"attribute": 5,
+					"value": "FEMALE"
+				}
+			],
+			"avatar_parts": [
+				{
+					"id": 209,
+					"name": "Set_Female_High-class-suit-02_Body",
+					"pattern": "FEMALE",
+					"part_type": 4
+				}
+			]
+		},
+		{
+			"id": 124,
+			"name": "Set_Female_Casual-Outfit_Body",
+			"image_url": "https://d1zyql4aht6ss7.cloudfront.net/images/avatars/F_NPC_01_2_1697790139515.png",
+			"attributes": [
+				{
+					"id": 376,
+					"created_at": "2023-10-24T11:01:31Z",
+					"updated_at": "2023-10-24T11:01:31Z",
+					"deleted_at": null,
+					"attribute": 5,
+					"value": "FEMALE"
+				}
+			],
+			"avatar_parts": [
+				{
+					"id": 210,
+					"name": "Set_Female_Casual-Outfit_Body",
+					"pattern": "FEMALE",
+					"part_type": 4
+				}
+			]
+		},
+		{
+			"id": 126,
+			"name": "Set_Male_Casual-suit-01_Body",
+			"image_url": "https://d1zyql4aht6ss7.cloudfront.net/images/avatars/M_NPC_01_3_1698145446574.png",
+			"attributes": [
+				{
+					"id": 382,
+					"created_at": "2023-10-24T11:05:17Z",
+					"updated_at": "2023-10-24T11:05:17Z",
+					"deleted_at": null,
+					"attribute": 5,
+					"value": "MALE"
+				}
+			],
+			"avatar_parts": [
+				{
+					"id": 214,
+					"name": "Set_Male_Casual-suit-01_Body",
+					"pattern": "MALE",
+					"part_type": 4
+				}
+			]
+		},
+		{
+			"id": 127,
+			"name": "Set_Male_Casual-suit-02_Body",
+			"image_url": "https://d1zyql4aht6ss7.cloudfront.net/images/avatars/M_NPC_01_5_1698145529656.png",
+			"attributes": [
+				{
+					"id": 384,
+					"created_at": "2023-10-24T11:05:37Z",
+					"updated_at": "2023-10-24T11:05:37Z",
+					"deleted_at": null,
+					"attribute": 5,
+					"value": "MALE"
+				}
+			],
+			"avatar_parts": [
+				{
+					"id": 198,
+					"name": "Set_Male_High-class-suit-02_Body",
+					"pattern": "MALE",
+					"part_type": 4
+				}
+			]
+		},
+		{
+			"id": 128,
+			"name": "Set_Male_Casual-outfit-01_Body",
+			"image_url": "https://d1zyql4aht6ss7.cloudfront.net/images/avatars/M_NPC_03_1698256618439.png",
+			"attributes": [
+				{
+					"id": 464,
+					"created_at": "2023-10-25T17:57:08Z",
+					"updated_at": "2023-10-25T17:57:08Z",
+					"deleted_at": null,
+					"attribute": 5,
+					"value": "MALE"
+				}
+			],
+			"avatar_parts": [
+				{
+					"id": 197,
+					"name": "Set_Male_High-class-suit-01_Body",
+					"pattern": "MALE",
+					"part_type": 4
+				}
+			]
+		},
+		{
+			"id": 454,
+			"name": "Set_Female_One-piece-outfit_Body",
+			"image_url": "https://d1zyql4aht6ss7.cloudfront.net/images/avatars/F_NPC_02_3_1698256668767.png",
+			"attributes": [
+				{
+					"id": 468,
+					"created_at": "2023-10-25T17:57:59Z",
+					"updated_at": "2023-10-25T17:57:59Z",
+					"deleted_at": null,
+					"attribute": 5,
+					"value": "FEMALE"
+				}
+			],
+			"avatar_parts": [
+				{
+					"id": 211,
+					"name": "Set_Female_One-piece-outfit_Body",
+					"pattern": "FEMALE",
+					"part_type": 4
+				}
+			]
+		},
+		{
+			"id": 458,
+			"name": "Set_Female_Dress-01_Body",
+			"image_url": "https://d1zyql4aht6ss7.cloudfront.net/images/avatars/F_NPC_05_3_1698256694673.png",
+			"attributes": [
+				{
+					"id": 470,
+					"created_at": "2023-10-25T17:58:25Z",
+					"updated_at": "2023-10-25T17:58:25Z",
+					"deleted_at": null,
+					"attribute": 5,
+					"value": "FEMALE"
+				}
+			],
+			"avatar_parts": [
+				{
+					"id": 201,
+					"name": "Set_Female_Dress-01_Body",
+					"pattern": "FEMALE",
+					"part_type": 4
+				}
+			]
+		},
+		{
+			"id": 459,
+			"name": "Set_Female_Dress-02_Body",
+			"image_url": "https://d1zyql4aht6ss7.cloudfront.net/images/avatars/F_NPC_03_3_1698257325866.png",
+			"attributes": [
+				{
+					"id": 490,
+					"created_at": "2023-10-25T18:09:12Z",
+					"updated_at": "2023-10-25T18:09:12Z",
+					"deleted_at": null,
+					"attribute": 5,
+					"value": "FEMALE"
+				}
+			],
+			"avatar_parts": [
+				{
+					"id": 202,
+					"name": "Set_Female_Dress-02_Body",
+					"pattern": "FEMALE",
+					"part_type": 4
+				}
+			]
+		},
+		{
+			"id": 460,
+			"name": "Set_Female_Dress-03_Body",
+			"image_url": "https://d1zyql4aht6ss7.cloudfront.net/images/avatars/F_NPC_05_2_1698256568950.png",
+			"attributes": [
+				{
+					"id": 460,
+					"created_at": "2023-10-25T17:56:18Z",
+					"updated_at": "2023-10-25T17:56:18Z",
+					"deleted_at": null,
+					"attribute": 5,
+					"value": "FEMALE"
+				}
+			],
+			"avatar_parts": [
+				{
+					"id": 203,
+					"name": "Set_Female_Dress-03_Body",
+					"pattern": "FEMALE",
+					"part_type": 4
+				}
+			]
+		},
+		{
+			"id": 461,
+			"name": "Set_Female_Dress-04_Body",
+			"image_url": "https://d1zyql4aht6ss7.cloudfront.net/images/avatars/F_NPC_04_1_1698257023453.png",
+			"attributes": [
+				{
+					"id": 488,
+					"created_at": "2023-10-25T18:03:47Z",
+					"updated_at": "2023-10-25T18:03:47Z",
+					"deleted_at": null,
+					"attribute": 5,
+					"value": "FEMALE"
+				}
+			],
+			"avatar_parts": [
+				{
+					"id": 204,
+					"name": "Set_Female_Dress-04_Body",
+					"pattern": "FEMALE",
+					"part_type": 4
+				}
+			]
+		},
+		{
+			"id": 462,
+			"name": "Set_Female_Trad-style-01_Body",
+			"image_url": "https://d1zyql4aht6ss7.cloudfront.net/images/avatars/F_NPC_03_2_1698256742455.png",
+			"attributes": [
+				{
+					"id": 474,
+					"created_at": "2023-10-25T17:59:12Z",
+					"updated_at": "2023-10-25T17:59:12Z",
+					"deleted_at": null,
+					"attribute": 5,
+					"value": "FEMALE"
+				}
+			],
+			"avatar_parts": [
+				{
+					"id": 205,
+					"name": "Set_Female_Trad-style-01_Body",
+					"pattern": "FEMALE",
+					"part_type": 4
+				}
+			]
+		},
+		{
+			"id": 463,
+			"name": "Set_Female_Formal-one-piece-01_Body",
+			"image_url": "https://d1zyql4aht6ss7.cloudfront.net/images/avatars/F_NPC_04_2_1698256761641.png",
+			"attributes": [
+				{
+					"id": 476,
+					"created_at": "2023-10-25T17:59:31Z",
+					"updated_at": "2023-10-25T17:59:31Z",
+					"deleted_at": null,
+					"attribute": 5,
+					"value": "FEMALE"
+				}
+			],
+			"avatar_parts": [
+				{
+					"id": 206,
+					"name": "Set_Female_Formal-one-piece-01_Body",
+					"pattern": "FEMALE",
+					"part_type": 4
+				}
+			]
+		},
+		{
+			"id": 464,
+			"name": "Set_Female_Formal-one-piece-02_Body",
+			"image_url": "https://d1zyql4aht6ss7.cloudfront.net/images/avatars/F_NPC_01_3_1698257449785.png",
+			"attributes": [
+				{
+					"id": 492,
+					"created_at": "2023-10-25T18:10:55Z",
+					"updated_at": "2023-10-25T18:10:55Z",
+					"deleted_at": null,
+					"attribute": 5,
+					"value": "FEMALE"
+				}
+			],
+			"avatar_parts": [
+				{
+					"id": 207,
+					"name": "Set_Female_Formal-one-piece-02_Body",
+					"pattern": "FEMALE",
+					"part_type": 4
+				}
+			]
+		},
+		{
+			"id": 479,
+			"name": "Set_Female_ClassSuilt-01_Body",
+			"image_url": "https://d1zyql4aht6ss7.cloudfront.net/images/avatars/F_NPC_04_3_1698145193542.png",
+			"attributes": [
+				{
+					"id": 370,
+					"created_at": "2023-10-24T11:00:05Z",
+					"updated_at": "2023-10-24T11:00:05Z",
+					"deleted_at": null,
+					"attribute": 5,
+					"value": "FEMALE"
+				}
+			],
+			"avatar_parts": [
+				{
+					"id": 208,
+					"name": "Set_Female_ClassSuilt-01_Body",
+					"pattern": "FEMALE",
+					"part_type": 4
+				}
+			]
+		},
+		{
+			"id": 481,
+			"name": "Set_Male_Casual-outfit-02_Body",
+			"image_url": "https://d1zyql4aht6ss7.cloudfront.net/images/avatars/M_NPC_03_1_1698256924481.png",
+			"attributes": [
+				{
+					"id": 484,
+					"created_at": "2023-10-25T18:02:16Z",
+					"updated_at": "2023-10-25T18:02:16Z",
+					"deleted_at": null,
+					"attribute": 5,
+					"value": "MALE"
+				}
+			],
+			"avatar_parts": [
+				{
+					"id": 196,
+					"name": "Set_Male_Casual-outfit-02_Body",
+					"pattern": "MALE",
+					"part_type": 4
+				}
+			]
+		},
+		{
+			"id": 482,
+			"name": "Set_Male_Casual-outfit-03_Body",
+			"image_url": "https://d1zyql4aht6ss7.cloudfront.net/images/avatars/M_NPC_03_3_1698256839796.png",
+			"attributes": [
+				{
+					"id": 506,
+					"created_at": "2023-10-26T06:36:12Z",
+					"updated_at": "2023-10-26T06:36:12Z",
+					"deleted_at": null,
+					"attribute": 5,
+					"value": "MALE"
+				}
+			],
+			"avatar_parts": [
+				{
+					"id": 212,
+					"name": "Set_Male_Casual-outfit-03_Body",
+					"pattern": "MALE",
+					"part_type": 4
+				}
+			]
+		},
+		{
+			"id": 483,
+			"name": "Set_Male_Casual-outfit-04_Body",
+			"image_url": "https://d1zyql4aht6ss7.cloudfront.net/images/avatars/M_NPC_05_1_1698257492460.png",
+			"attributes": [
+				{
+					"id": 494,
+					"created_at": "2023-10-25T18:11:36Z",
+					"updated_at": "2023-10-25T18:11:36Z",
+					"deleted_at": null,
+					"attribute": 5,
+					"value": "MALE"
+				}
+			],
+			"avatar_parts": [
+				{
+					"id": 213,
+					"name": "Set_Male_Casual-outfit-04_Body",
+					"pattern": "MALE",
+					"part_type": 4
+				}
+			]
+		},
+		{
+			"id": 484,
+			"name": "Set_Male_High-class-suit-01_Body",
+			"image_url": "https://d1zyql4aht6ss7.cloudfront.net/images/avatars/M_NPC_01_1_1698145656475.png",
+			"attributes": [
+				{
+					"id": 456,
+					"created_at": "2023-10-25T17:54:53Z",
+					"updated_at": "2023-10-25T17:54:53Z",
+					"deleted_at": null,
+					"attribute": 5,
+					"value": "MALE"
+				}
+			],
+			"avatar_parts": [
+				{
+					"id": 197,
+					"name": "Set_Male_High-class-suit-01_Body",
+					"pattern": "MALE",
+					"part_type": 4
+				}
+			]
+		},
+		{
+			"id": 485,
+			"name": "Set_Male_High-class-suit-02_Body",
+			"image_url": "https://d1zyql4aht6ss7.cloudfront.net/images/avatars/M_NPC_02_8_1698256516656.png",
+			"attributes": [
+				{
+					"id": 458,
+					"created_at": "2023-10-25T17:55:29Z",
+					"updated_at": "2023-10-25T17:55:29Z",
+					"deleted_at": null,
+					"attribute": 5,
+					"value": "MALE"
+				}
+			],
+			"avatar_parts": [
+				{
+					"id": 198,
+					"name": "Set_Male_High-class-suit-02_Body",
+					"pattern": "MALE",
+					"part_type": 4
+				}
+			]
+		},
+		{
+			"id": 486,
+			"name": "Set_Male_Tuxedo-01_Body",
+			"image_url": "https://d1zyql4aht6ss7.cloudfront.net/images/avatars/M_NPC_02_5_1698256816130.png",
+			"attributes": [
+				{
+					"id": 480,
+					"created_at": "2023-10-25T18:00:25Z",
+					"updated_at": "2023-10-25T18:00:25Z",
+					"deleted_at": null,
+					"attribute": 5,
+					"value": "MALE"
+				}
+			],
+			"avatar_parts": [
+				{
+					"id": 199,
+					"name": "Set_Male_Tuxedo-01_Body",
+					"pattern": "MALE",
+					"part_type": 4
+				}
+			]
+		},
+		{
+			"id": 487,
+			"name": "Set_Male_Tuxedo-02_Body",
+			"image_url": "https://d1zyql4aht6ss7.cloudfront.net/images/avatars/M_NPC_01_1698256960657.png",
+			"attributes": [
+				{
+					"id": 486,
+					"created_at": "2023-10-25T18:02:49Z",
+					"updated_at": "2023-10-25T18:02:49Z",
+					"deleted_at": null,
+					"attribute": 5,
+					"value": "MALE"
+				}
+			],
+			"avatar_parts": [
+				{
+					"id": 200,
+					"name": "Set_Male_Tuxedo-02_Body",
+					"pattern": "MALE",
+					"part_type": 4
+				}
+			]
+		},
+		{
+			"id": 518,
+			"name": "質ヲイクホ望山提変フネ争索とを公情タヲユ助熱ば百館競オ吉2市ノフヲケ標条ん府龍ルサユ触得息曲燃まぽほ。記がぱじ一剥え表再ましね威68火9権ぜりち導供重てスイ更賞ケレトシ法和市国だへきく論承ゅほ。特ヤヌヱヘ子量わ天能ネケサ持業軽フヨラ専質けでず楽2春ンるれび図資かと広応に賞10柴ヌ近分む集特そ権改ゆ物応規ネナ禄刊くがむだ性稲那久伊すだむ。  北ラロチ復明ッよぱ合保キ作宅セ天湾鹿リッぎ各転シヘリエ表人クぼ市営重ラヨ原味ぞわぐト具善イ文近らど詰紙え最石緊ぜめえや沢争各戸証拡盛ろトけ。保せレま囲図シカヌ提野12",
+			"image_url": "https://d1zyql4aht6ss7.cloudfront.net/images/avatars/Rectangle%203847_1692842754058_1698302765456.png",
+			"attributes": [
+				{
+					"id": 508,
+					"created_at": "2023-10-26T06:47:28Z",
+					"updated_at": "2023-10-26T06:47:28Z",
+					"deleted_at": null,
+					"attribute": 5,
+					"value": "MALE"
+				}
+			],
+			"avatar_parts": [
+				{
+					"id": 215,
+					"name": "Set_Male_Casual-suit-02_Body",
+					"pattern": "MALE",
+					"part_type": 4
+				}
+			]
+		}
+	]
+}
