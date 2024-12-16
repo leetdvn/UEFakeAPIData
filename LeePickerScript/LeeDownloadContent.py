09a6e31a2613f7063218e1227eb5d6258f510215
leetdvn <111244237+leetdvn@users.noreply.github.com>
2024-12-17 03:01:16 +0700

    update

--- LeePickerScript/LeeDownloadContent.py
+++ LeePickerScript/LeeDownloadContent.py
@@ -1,9 +1,11 @@
 import urllib
 import json
+import base64
 
 #url='https://api.github.com/repos/MrLeeTa/leePicker/src/git/trees/master?recursive=1'
 url='https://api.github.com/repos/MrLeeTa/icontemp/git/trees/master?recursive=1'
-filename='C:/Users/thang/Documents/GitHub/UEFakeAPIData/IMAGE/abc.json'
+imgUrl='https://api.github.com/repos/MrLeeTa/icontemp/git/blobs/ad199c18e92d8dd5fd514a327095c9c24c8b5aa1'
+filename='C:/Users/thang/Documents/GitHub/UEFakeAPIData/IMAGE/testImg.png'
 abc =urllib.urlopen(url)
 #data=data.decode("utf-8")
 jsdata=json.load(abc)
@@ -23,3 +25,10 @@ print(json.dumps(jsdata,indent=4))
 # #print("number : ",len(contents))
 # for elm in contents:
 #     print(elm)
+imgdata = urllib.urlopen(imgUrl)
+imgjson=json.load(imgdata)
+content = imgjson.get("content")
+img = base64.b64decode(content)
+# with open(filename,'wb') as f:
+#     f.write(img)
+#     f.close()
